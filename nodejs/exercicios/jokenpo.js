af0a314e16c29e817cedfa73d6c6d53a1caf18e7
Vinicius Nascimento <115954568+spvinicius@users.noreply.github.com>
2023-09-06 19:06:16 -0300

    Update jokenpo.js

---
+++ nodejs/exercicios/jokenpo.js
@@ -0,0 +1,17 @@
+/**
+ * Jokenpo
+ */
+
+const read = require('readline-sync')
+const colors = require("colors")
+
+console.clear()
+console.log("-----------Jokenpo----------------")
+console.log("1. Pedra".italic.blue)
+console.log("2.papel".italic.white)
+console.log("3. tesoura".italic.red)
+opcao = Number(read.question("Escolha um sistema operacional: ".italic))
+
+if (""){
+
+}
